d54f1dd90b4bf44caf3e2eb04eb828d40dbdcd8c
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-12-29 09:03:40 +0530

    First Commit

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return new MaterialApp(
+      title: 'Weather App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(
+        title: 'Weather App',
+      ),
+    );
+  }
+
+}
