ca79571e423438f1052bc9bbef5d22fc90173304
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-12-29 09:30:50 +0530

    Created Weather.dart

--- weather_app/lib/ui/Weather.dart
+++ weather_app/lib/ui/Weather.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+class Weather extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(60.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
 
+         ],
+        ),
+    );
+  }
+
+}
 
