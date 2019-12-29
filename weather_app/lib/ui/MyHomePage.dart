0b1825733bc653cca6818b9bc0f0f7739de9d528
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-12-29 09:11:44 +0530

    Second commit

--- weather_app/lib/ui/MyHomePage.dart
+++ weather_app/lib/ui/MyHomePage.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+class MyHomePage extends StatefulWidget{
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text(
+          widget.title
+         ),
+        ),
+    );
+  }
+
+} 
