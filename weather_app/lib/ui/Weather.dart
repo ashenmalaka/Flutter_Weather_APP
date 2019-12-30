ef0310a68c0381afbc2cc04fa9135c70c5a0b698
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-12-30 22:33:34 +0530

    Added date

--- weather_app/lib/ui/Weather.dart
+++ weather_app/lib/ui/Weather.dart
@@ -4,6 +4,17 @@ import 'package:intl/intl.dart';
 class Weather extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+
+    Widget dataSection = Container(
+      child: Text(
+        DateFormat('MMMM d, H:m').format(DateTime.now()),
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 24.0
+          )
+      ),
+    );
     return Container(
       padding: const EdgeInsets.all(60.0),
       child: Column(
