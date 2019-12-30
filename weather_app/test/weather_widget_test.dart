7c9c69f729e4cb58c40ea8eca028ff865f2ebb9f
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-12-30 23:27:25 +0530

    Created weather widget test dart file

---
+++ weather_app/test/weather_widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:weather_app/ui/Weather.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  
+}
