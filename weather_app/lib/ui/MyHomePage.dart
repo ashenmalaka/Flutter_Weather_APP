ef9649f273e5d134403509e1cc20b67dfc6a6d8c
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-09 23:03:48 +0530

    Added codes to myHomePage.dart to connect netowrk with main page

--- weather_app/lib/ui/MyHomePage.dart
+++ weather_app/lib/ui/MyHomePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './Weather.dart';
+import 'package:weather_app/model/WeatherData.dart';
+import 'package:weather_app/api/MapApi.dart';
 
 class MyHomePage extends StatefulWidget{
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -12,6 +14,16 @@ class MyHomePage extends StatefulWidget{
 }
 
 class _MyHomePageState extends State<MyHomePage>{
+
+  WeatherData _weatherData;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -24,6 +36,18 @@ class _MyHomePageState extends State<MyHomePage>{
 
         body: Weather(),
     );
+  }
+
+  getCurrentLocation(){
+    loadWeather(lat: 40.71, lon: -74.01);
+  }
+
+  loadWeather({double lat, double lon}) async {
+    MapApi mapApi = MapApi.getInstance();
+    final data = await mapApi.getWeather(lat: lat, lon: lon);
+    setState(() {
+      this._weatherData = data;
+    });
   }
 
 } 
