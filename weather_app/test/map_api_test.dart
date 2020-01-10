26c060a3aeb464e312f8cf43c740d87c61bb646b
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-10 23:18:08 +0530

    Added unit testing for map api

---
+++ weather_app/test/map_api_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main(){
+  test('MapApi getWeather method', () async{
+    final double lat = 35.67;
+    final double lon = 136.77;
+
+    final mapJson = 
+  });
+}
